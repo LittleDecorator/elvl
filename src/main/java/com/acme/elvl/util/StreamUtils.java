a63183ecd647dab52b1b54fc9923d9b3df4b8203
Nikolai Kobzev <Nikolai_Kobzev@epam.com>
2020-04-22 12:33:26 +0300

    style

--- src/main/java/com/acme/elvl/util/StreamUtils.java
+++ src/main/java/com/acme/elvl/util/StreamUtils.java
@@ -8,18 +8,18 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class StreamUtils {
 
-    public <T> Predicate<T> logFiltered(Predicate<T> predicate, Consumer<T> action) {
-        Objects.requireNonNull(predicate);
-        Objects.requireNonNull(action);
+  public <T> Predicate<T> logFiltered(Predicate<T> predicate, Consumer<T> action) {
+    Objects.requireNonNull(predicate);
+    Objects.requireNonNull(action);
 
-        return value -> {
-            if (predicate.test(value)) {
-                return true;
-            } else {
-                action.accept(value);
-                return false;
-            }
-        };
-    }
+    return value -> {
+      if (predicate.test(value)) {
+        return true;
+      } else {
+        action.accept(value);
+        return false;
+      }
+    };
+  }
 
 }
